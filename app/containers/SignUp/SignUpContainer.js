4c5eacc182fdb1680138f6e88d56d4e95bb40f1c
Aristides Perez <aris.a.perez@gmail.com>
2017-12-10 16:11:22 -0500

    Connect Redux and setup auth procedure

--- app/containers/SignUp/SignUpContainer.js
+++ app/containers/SignUp/SignUpContainer.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as userActionCreators from 'redux/modules/users'
 import { SignUp } from 'components'
 import auth from 'helpers/auth'
 
 class SignUpContainer extends Component {
-  handleAuth () {
+  handleAuth = () => {
+    this.props.fetchingUser()
     auth().then((user) => {
-      console.log('Authed User', user)
+      this.props.fetchingUserSuccess(user.uid, user, Date.now())
+      this.props.authUser(user.uid)
     })
+      .catch((error) => {
+        this.props.fetchingUserError(error)
+      })
   }
   render () {
     return (
       <SignUp
-        isFetching={false}
-        error=''
+        isFetching={this.props.isFetching}
+        error={this.props.error}
         onAuth={this.handleAuth}/>
     )
   }
 }
 
-export default SignUpContainer
+const mapStateToProps = (state) => {
+  return {
+    isFetching: state.isFetching,
+    error: state.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(userActionCreators, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUpContainer)
+
+SignUpContainer.propTypes = {
+  isFetching: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
+  authUser: PropTypes.func.isRequired,
+  fetchingUser: PropTypes.func.isRequired,
+  fetchingUserSuccess: PropTypes.func.isRequired,
+  fetchingUserError: PropTypes.func.isRequired
+}
--- app/index.js
+++ app/index.js
@@ -1,7 +1,15 @@
+import React from 'react'
 import ReactDom from 'react-dom'
 import routes from './config/routes'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import users from 'redux/modules/users'
+
+const store = createStore(users)
 
 ReactDom.render(
-  routes,
+  <Provider store={store}>
+    {routes}
+  </Provider>,
   document.getElementById('app')
 )
